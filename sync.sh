493719fe1b9e5401ff6126077746411d3b79579f
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-12-30 11:01:23 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,5 +5,5 @@ rm -rf .repo/local_manifests
 git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # Run build script 
-chmod u+x /scripts/build.sh
+chmod u+x scripts/build.sh
 ./scripts/build.sh
