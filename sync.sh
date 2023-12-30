11a4522c88e84ec37199a41d17b8755b17b1fa5c
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-12-30 08:00:06 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+#Remove local manifest
+rm -rf .repo/local_manifests
+#Clone trees local manifest
+git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+# Run build script 
+~/scripts/build.sh
