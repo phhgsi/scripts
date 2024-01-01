779d3218fec6d4960f85aabc6f2d88a8db840dee
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-01 19:28:55 +0530

    Update derpfest14.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+rm -rf .repo
+rm -rf *
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+./scripts/sync.sh
