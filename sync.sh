b5a09595ac81e522eda5aef0ea0aacf45e49c1ff
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-02 12:28:27 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,6 +4,8 @@ rm -rf .repo/local_manifests
 #Clone trees local manifest
 git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#remove rm -rf hardware/google/pixel/kernel_headers
+rm -rf hardware/google/pixel/kernel_headers
 # Run build script 
 chmod u+x scripts/build.sh
 ./scripts/build.sh
