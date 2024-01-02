d9c3496b46244e6743eebba2f24d2c960e6c8720
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-03 00:22:43 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+echo
+echo "--------------------------------------"
+echo "    derpfest14 14.0 Buildbot          "
+echo "                  by                  "
+echo "            ajaysinghsati             "
+echo "--------------------------------------"
+echo
 #Remove local manifest
 rm -rf .repo/local_manifests
 #Clone trees local manifest
@@ -6,7 +14,6 @@ git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #remove rm -rf hardware/google/pixel/kernel_headers
 rm -rf hardware/google/pixel/kernel_headers
-rm -rf hardware/oplus
 # Run build script 
 chmod u+x scripts/build.sh
 ./scripts/build.sh
