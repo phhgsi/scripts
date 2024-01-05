d7e102fc638b54fbf2097b180d077e234dd476c0
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-05 23:06:24 +0530

    Update derpfest14.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -4,5 +4,10 @@ rm -rf .repo
 rm -rf *
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-chmod u+x scripts/sync.sh
-./scripts/sync.sh
+#Remove local manifest
+rm -rf .repo/local_manifests
+#Clone trees local manifest
+git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#remove rm -rf hardware/google/pixel/kernel_headers
+rm -rf hardware/google/pixel/kernel_headers
