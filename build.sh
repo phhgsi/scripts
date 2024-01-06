d2906cc24c2e269d481fa302c417d53b4d58f829
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-06 15:15:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,20 +7,4 @@ export TZ=Asia/Dhaka
 #make command
 mka derp
 # Check build status and display animation
-if [ $? -eq 0 ]; then
-    echo "Build successful!"
-    printf "\e[?25l"  # Hide cursor
-    cat <<EOF
-        _____
-      .'* *.'
-     / _  _ \
-    |(o|o)| |
-      \___/
-   Build by Ajay
-       with ⚡
- EOF
-    figlet -f slant "Lightning Dart"
-    printf "\e[?25h"  # Show cursor
-else
-    echo "Build failed. Please check the logs for errors."
-fi
+
