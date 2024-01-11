16c2de3a27ef1daf9f0bca8f1eae3a29f81f2789
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-11 16:28:34 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,14 +9,14 @@ echo "--------------------------------------"
 echo
 #Remove local manifest
 rm -rf .repo/local_manifests
-rm -rf device/oneplus/oscar
+#rm -rf device/oneplus/oscar
+#rm -rf vendor/oplus/camera
 rm -rf vendor/oplus/camera
-rm -rf hardware/oplus
 #Clone trees local manifest
-git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
+#repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #remove rm -rf hardware/google/pixel/kernel_headers
-git clone https://gitlab.com/PixelOS-Devices/proprietary_vendor_oplus_camera.git -b 14.0 vendor/oplus/camera
+git clone https://gitlab.com/PixelOS-Devices/proprietary_vendor_oplus_camera.git -b 13.0 vendor/oplus/camera
 rm -rf hardware/google/pixel/kernel_headers
 # Run build script 
 chmod u+x scripts/build.sh
