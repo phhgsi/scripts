018008de245d6084f330287d4b734e623259de86
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-11 08:59:01 +0530

    Update derpfest14.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -25,21 +25,3 @@ lunch derp_oscar-user
 export TZ=Asia/Dhaka
 #make command
 mka derp
-# Check build status and display animation
-if [ $? -eq 0 ]; then
-    echo "Build successful!"
-    printf "\e[?25l"  # Hide cursor
-    cat <<EOF
-        _____
-      .'* *.'
-     / _  _ \
-    |(o|o)| |
-      \___/
-   Build by Ajay
-       with ⚡
- EOF
-    figlet -f slant "Lightning Dart"
-    printf "\e[?25h"  # Show cursor
-else
-    echo "Build failed. Please check the logs for errors."
-fi
