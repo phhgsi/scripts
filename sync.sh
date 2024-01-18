7ba18db5f973d7ef0cf04799417ce1309d860c49
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-18 00:37:03 -0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -14,9 +14,8 @@ rm -rf .repo/local_manifests
 rm -rf vendor/oplus/camera
 #Clone trees local manifest
 #git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
-#repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-#remove rm -rf hardware/google/pixel/kernel_headers
-git clone https://gitlab.com/ajaysinghsati/proprietary_vendor_oplus_camera.git -b 13.0 vendor/oplus/camera
+#repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
+git clone https://gitlab.com/cjh1249131356/vendor_oneplus_camera.git -b fourteen vendor/oneplus/camera
 rm -rf hardware/google/pixel/kernel_headers
 # Run build script 
 chmod u+x scripts/build.sh
