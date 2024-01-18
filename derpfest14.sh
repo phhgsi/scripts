e3e03b32f337d8fba4463d3aaa1ec38ca84d9dc4
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-18 00:33:22 -0800

    Update derpfest14.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -10,18 +10,6 @@ rm -rf .repo
 rm -rf *
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-#Remove local manifest
-rm -rf .repo/local_manifests
-#Clone trees local manifest
-git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-#remove rm -rf hardware/google/pixel/kernel_headers
-rm -rf hardware/google/pixel/kernel_headers
-#!/bin/bash
-#envsetup
-source build/envsetup.sh
-# lunch command 
-lunch derp_oscar-user
-export TZ=Asia/Dhaka
-#make command
-mka derp
+git clone https://github.com/phhgsi/scripts.git 
+chmod u+x scripts/sync.sh
+./scripts/sync.sh
