6819e744969a91d3ec410af6ad6f211406eeea46
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-21 11:51:24 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -16,7 +16,7 @@ rm -rf device/oneplus/sm6375-common
 #Clone trees local manifest
 git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-#git clone https://gitlab.com/ajay24ce008/propriotary_camera.git vendor/oneplus/camera
+git clone https://github.com/afterlifeoscar/oplus_cam.git vendor/oplus/camera
 rm -rf hardware/google/pixel/kernel_headers
 # Run build script 
 chmod u+x scripts/build.sh
