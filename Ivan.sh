e331053c8db394d17d3a1c83a2df6edc6bd9393c
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-23 21:42:44 +0530

    Update Ivan.sh

--- Ivan.sh
+++ Ivan.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 git clone https://github.com/phhgsi/device_oneplus_ivan.git device/oneplus/ivan
+#envsetup
+source build/envsetup.sh
+# lunch command 
+lunch derp_ivan-userdebug
+export TZ=Asia/Dhaka
+#make command
+mka derp
+# Check build status and display animation
