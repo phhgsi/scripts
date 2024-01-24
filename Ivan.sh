e02a86ae7f25ad0c4385c7e08f1e4118c9dd61db
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-24 08:22:08 +0530

    Update Ivan.sh

--- Ivan.sh
+++ Ivan.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -rf kernel/oneplus/ivan
 rm -rf device/oneplus/ivan
 rm -rf vendor/oneplus/ivan
 git clone https://github.com/phhgsi/device_oneplus_ivan.git device/oneplus/ivan
 git clone https://github.com/phhgsi/vendor_oneplus_ivan.git vendor/oneplus/ivan
+git clone https://github.com/oneplus-mt6877/android_kernel_oneplus_ivan_4.14.git -b main kernel/oneplus/ivan
 rm -rf hardware/oplus/hidl/touch
 #envsetup
 source build/envsetup.sh
