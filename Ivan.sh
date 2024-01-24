85d8f8be773e5128d52832852be0399cf41a76a2
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-24 11:56:17 +0530

    Update Ivan.sh

--- Ivan.sh
+++ Ivan.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#rm -rf kernel/oneplus/ivan
+rm -rf kernel/oneplus/ivan
 rm -rf device/oneplus/ivan
 #rm -rf vendor/oneplus/ivan
 git clone https://github.com/phhgsi/device_oneplus_ivan.git device/oneplus/ivan
-#git clone https://github.com/phhgsi/vendor_oneplus_ivan.git vendor/oneplus/ivan
-#git clone https://github.com/oneplus-mt6877/android_kernel_oneplus_ivan_4.14.git -b main kernel/oneplus/ivan
+git clone https://github.com/phhgsi/vendor_oneplus_ivan.git vendor/oneplus/ivan
+git clone https://github.com/PQEnablers-Devices/android_kernel_mediatek_4.19.git kernel/oneplus/ivan
 rm -rf hardware/oplus/hidl/touch
 #envsetup
 source build/envsetup.sh
 # lunch command 
-lunch derp_ivan-userdebug
+lunch lineage_ivan-userdebug
 export TZ=Asia/Dhaka
 #make command
-mka derp
+mka bacon
 # Check build status and display animation
