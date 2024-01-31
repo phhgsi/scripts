ce8a61377ae828b090c2d8a32a644d37612dadad
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-31 12:48:42 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,23 +8,24 @@ echo "            ajaysinghsati             "
 echo "--------------------------------------"
 echo
 #rm -rf device/oneplus/oscar
-git clone https://github.com/phhgsi/device_oneplus_oscar.git device/oneplus/oscar
+#git clone https://github.com/phhgsi/device_oneplus_oscar.git device/oneplus/oscar
 #rm -rf device/oneplus/sm6375-common
-git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+#git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
 #rm -rf kernel/oneplus/sm6375
-git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-12 kernel/oneplus/sm6375
+#git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-12 kernel/oneplus/sm6375
 #rm -rf vendor/oneplus/oscar
-git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
+#git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
 #rm -rf vendor/oneplus/sm6375-common
-git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
+#git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
 #rm -rf hardware/oplus
-git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
+#git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
 
-git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+#git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
 #rm -rf vendor/oplus/camera
-git clone https://github.com/phhgsi/oplus_cam.git -b main vendor/oplus/camera
-git clone https://www.opencode.net/romdumprx/android_vendor_oplus_camera.git vendor/oplus/cam
-git clone https://www.opencode.net/romdumprx/gcam.git -b udc vendor/gcam
+git clone https://www.opencode.net/romdumprx/procam.git vendor/oplus/camera
+rm -rf vendor/gcam
+git clone https://www.opencode.net/romdumprx/gcam.git -b udc vendor/oplus/gcam
+git clone https://github.com/ajaysinghsati/android_vendor_oplus_camera.git vendor/oplus/lib
 
 #git clone https://github.com/DerpFest-AOSP/frameworks_base.git frameworks/base
 #git clone https://github.com/DerpFest-AOSP/frameworks_av.git frameworks/av
