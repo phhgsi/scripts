573bbcd2a79382aa119e447bb21e5cee951966ae
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-01-31 20:47:58 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -23,7 +23,7 @@ echo
 #git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
 #rm -rf vendor/oplus/cam
 rm -rf vendor/oplus/camera
-git clone https://www.opencode.net/romdumprx/procam.git vendor/oplus/camera
+git clone https://gitlab.com/romdump/proprietary_vendor_oplus_camera.git vendor/oplus/camera
 #rm -rf vendor/gcam
 #git clone https://www.opencode.net/romdumprx/gcam.git vendor/oplus/gcam
 rm -rf vendor/oplus/lib
