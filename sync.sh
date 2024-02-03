bf1376b52d746329068aacb42f489aac24e882b4
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-03 09:00:32 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,21 +7,21 @@ echo "                  by                  "
 echo "            ajaysinghsati             "
 echo "--------------------------------------"
 echo
-#rm -rf device/oneplus/oscar
-#git clone https://github.com/phhgsi/device_oneplus_oscar.git device/oneplus/oscar
+rm -rf device/oneplus/oscar
+git clone https://github.com/phhgsi/device_oneplus_oscar.git device/oneplus/oscar
 rm -rf device/oneplus/sm6375-common
 git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
-#rm -rf kernel/oneplus/sm6375
-#git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-12 kernel/oneplus/sm6375
-#rm -rf vendor/oneplus/oscar
-#git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
-#rm -rf vendor/oneplus/sm6375-common
-#git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
-#rm -rf hardware/oplus
-#git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
+rm -rf kernel/oneplus/sm6375
+git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-12 kernel/oneplus/sm6375
+rm -rf vendor/oneplus/oscar
+git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
+rm -rf vendor/oneplus/sm6375-common
+git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
+rm -rf hardware/oplus
+git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
 
-#git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
-#rm -rf vendor/oplus/cam
+git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+rm -rf vendor/oplus/cam
 rm -rf vendor/oplus/camera
 git clone https://gitlab.com/PixelOS-Devices/proprietary_vendor_oplus_camera.git vendor/oplus/camera
 #rm -rf vendor/oplus/gcam
