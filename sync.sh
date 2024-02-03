2816e3e7d24e58c99225bd08dc38e2652e937f93
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-03 08:34:43 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -23,11 +23,11 @@ git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/onep
 #git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
 #rm -rf vendor/oplus/cam
 rm -rf vendor/oplus/camera
-git clone https://gitlab.com/romdump/proprietary_vendor_oplus_camera.git vendor/oplus/camera
+git clone https://gitlab.com/PixelOS-Devices/proprietary_vendor_oplus_camera.git vendor/oplus/camera
 #rm -rf vendor/oplus/gcam
 #git clone https://www.opencode.net/romdumprx/gcam.git vendor/oplus/gcam
-#rm -rf vendor/oplus/lib
-#git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
+rm -rf vendor/oplus/lib
+git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
 
 #git clone https://github.com/DerpFest-AOSP/frameworks_base.git frameworks/base
 #git clone https://github.com/DerpFest-AOSP/frameworks_av.git frameworks/av
