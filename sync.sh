2cdec9b746a38b727441915f7dcd9b324e9268b1
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-03 20:52:33 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -14,13 +14,15 @@ git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/onep
 #rm -rf kernel/oneplus/sm6375
 #git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-12 kernel/oneplus/sm6375
 rm -rf vendor/oneplus/oscar
-git clone https://github.com/derpfestoscar/android_vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
+git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
 #rm -rf vendor/oneplus/sm6375-common
 #git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
 #rm -rf hardware/oplus
 #git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
 # rm -rf 
 #git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+rm -rf vendor/oplus/lib
+git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
 rm -rf vendor/oplus/camera
 git clone https://gitlab.com/romdump/proprietary_vendor_oplus_camera.git vendor/oplus/camera
 
