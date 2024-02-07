b79ab015f783764e9c6efc2b972ae9ddd6396ab8
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-07 15:21:37 +0530

    Update derpfest14.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -8,8 +8,8 @@ echo "--------------------------------------"
 echo
 rm -rf .repo
 rm -rf *
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-git clone https://github.com/phhgsi/scripts.git 
-chmod u+x scripts/sync.sh
-./scripts/sync.sh
+repo init --depth=1 -u https://github.com/Project-Elixir/manifest -b UNO
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+#git clone https://github.com/phhgsi/scripts.git 
+#chmod u+x scripts/sync.sh
+#./scripts/sync.sh
