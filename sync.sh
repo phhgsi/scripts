080685d077f55144ae75aa397205198b85a9913c
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-14 23:13:33 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,26 +7,26 @@ echo "                  by                  "
 echo "            ajaysinghsati             "
 echo "--------------------------------------"
 echo
-rm -rf device/oneplus/oscar
-git clone https://github.com/derpfestoscar/device_oneplus_oscar.git device/oneplus/oscar
-rm -rf device/oneplus/sm6375-common
-git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
-rm -rf kernel/oneplus/sm6375
-git clone https://github.com/derpfestoscar/android_kernel_oneplus_sm6375.git -b holi-12 kernel/oneplus/sm6375
-rm -rf vendor/oneplus/oscar
-git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
-rm -rf vendor/oneplus/sm6375-common
-git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
-rm -rf hardware/oplus
-git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
-#rm -rf 
-#git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
-rm -rf vendor/oplus/lib
-git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
-rm -rf vendor/oplus/camera
-git clone https://gitlab.com/romdump/proprietary_vendor_oplus_camera.git -b lineage-21 vendor/oplus/camera
-rm -rf prebuilts/derp
-git clone https://github.com/derpfestoscar/prebuilts_derp.git prebuilts/derp
+#rm -rf device/oneplus/oscar
+#git clone https://github.com/derpfestoscar/device_oneplus_oscar.git device/oneplus/oscar
+#rm -rf device/oneplus/sm6375-common
+#git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
+#rm -rf kernel/oneplus/sm6375
+#git clone https://github.com/derpfestoscar/android_kernel_oneplus_sm6375.git -b holi-12 kernel/oneplus/sm6375
+#rm -rf vendor/oneplus/oscar
+#git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
+#rm -rf vendor/oneplus/sm6375-common
+#git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
+#rm -rf hardware/oplus
+#git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
+rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
+git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+#rm -rf vendor/oplus/lib
+#git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
+#rm -rf vendor/oplus/camera
+#git clone https://gitlab.com/romdump/proprietary_vendor_oplus_camera.git -b lineage-21 vendor/oplus/camera
+#rm -rf prebuilts/derp
+#git clone https://github.com/derpfestoscar/prebuilts_derp.git prebuilts/derp
 
 #git clone https://github.com/DerpFest-AOSP/frameworks_base.git frameworks/base
 #git clone https://github.com/DerpFest-AOSP/frameworks_av.git frameworks/av
