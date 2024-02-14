bef8830e1984dbcfea7f6bb896c3ea3ab67ad528
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-02-15 00:35:11 +0530

    Update derpfest14.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -7,6 +7,7 @@ echo "            ajaysinghsati             "
 echo "--------------------------------------"
 echo
 rm -rf .repo
+rm -rf *
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 git clone https://github.com/phhgsi/scripts.git && chmod u+x scripts/sync.sh && ./scripts/sync.sh
