3c4b4dd8d1a2c3e2f8c180672d4f5616ba1498e7
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-03-14 21:16:53 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -12,19 +12,19 @@ git clone https://github.com/derpfestoscar/device_oneplus_oscar.git device/onepl
 rm -rf device/oneplus/sm6375-common
 git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git device/oneplus/sm6375-common
 rm -rf kernel/oneplus/sm6375
-git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-16 kernel/oneplus/sm6375
+git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-larry kernel/oneplus/sm6375
 rm -rf vendor/oneplus/oscar
 git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
 rm -rf vendor/oneplus/sm6375-common
 git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
 rm -rf hardware/oplus
 git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
-#rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
-#git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
+git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
 rm -rf vendor/oplus/lib
-git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
+#git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
 rm -rf vendor/oplus/camera
-git clone https://gitlab.com/ajay24ce008/proprietary_vendor_oplus_camera.git -b lineage-21-op12 vendor/oplus/camera
+#git clone https://gitlab.com/ajay24ce008/proprietary_vendor_oplus_camera.git -b lineage-21-op12 vendor/oplus/camera
 rm -rf prebuilts/derp
 git clone https://github.com/derpfestoscar/prebuilts_derp.git prebuilts/derp
 
