0b187aff4a8f4c39ba4bd281b487b52785d5fd53
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-03-24 21:04:53 +0530

    Update superior.sh

--- superior.sh
+++ superior.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+echo
+echo "--------------------------------------"
+echo "    Superior extended 14.0 Buildbot   "
+echo "                  by                  "
+echo "            ajaysinghsati             "
+echo "--------------------------------------"
+echo
+rm -rf .repo
+rm -rf *
+repo init -u https://github.com/SuperiorExtended/manifest -b UDC --git-lfs
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+git clone https://github.com/phhgsi/scripts.git && chmod u+x scripts/superior/sync.sh && ./scripts/superior/sync.sh
