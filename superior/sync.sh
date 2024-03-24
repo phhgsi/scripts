40b35f59fc6849d9c0096bd242315d2cfcf01159
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-03-24 21:27:15 +0530

    Update sync.sh

--- superior/sync.sh
+++ superior/sync.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+echo
+echo "--------------------------------------"
+echo "    superior 14.0 Buildbot            "
+echo "                  by                  "
+echo "            ajaysinghsati             "
+echo "--------------------------------------"
+echo
+rm -rf device/oneplus/oscar
+git clone https://github.com/phhhsi/device_oneplus_oscar.git -b superior device/oneplus/oscar
+rm -rf device/oneplus/sm6375-common
+git clone https://github.com/phhgsi/device_oneplus_sm6375-common.git -b superior device/oneplus/sm6375-common
+rm -rf kernel/oneplus/sm6375
+git clone https://github.com/phhgsi/android_kernel_oneplus_sm6375.git -b holi-16 kernel/oneplus/sm6375
+rm -rf vendor/oneplus/oscar
+git clone https://github.com/phhgsi/vendor_oneplus_oscar.git -b fourteen vendor/oneplus/oscar
+rm -rf vendor/oneplus/sm6375-common
+git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
+rm -rf hardware/oplus
+git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-holi hardware/oplus
+rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
+git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+rm -rf vendor/oplus/lib
+git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
+rm -rf vendor/oplus/camera
+git clone https://gitlab.com/ajay24ce008/proprietary_vendor_oplus_camera.git -b lineage-21 vendor/oplus/camera
+rm -rf prebuilts/derp
+rm -rf hardware/google/pixel/kernel_headers
+# Run build script 
+chmod u+x scripts/superior/build.sh
+./scripts/superior/build.sh
