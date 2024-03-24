4f530429e87bde84470b5918537b5fa7b4384abb
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-03-24 21:28:52 +0530

    Update build.sh

--- superior/build.sh
+++ superior/build.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+#envsetup
+source build/envsetup.sh
+# lunch command 
+lunch superior_oscar-eng
+export TZ=Asia/Dhaka
+#make command
+mka bacon
+# Check build status and display animation
