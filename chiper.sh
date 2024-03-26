5ccb49ce0a163ef2c2291ad0ec069598311d4564
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-03-26 19:59:27 +0530

    Update chiper.sh

--- chiper.sh
+++ chiper.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-git clone https://github.com/phhgsi/scripts.git && chmod u+x scripts/chiper/sync.sh && ./scripts/chiper/sync.sh
+rm -rf scripts && git clone https://github.com/phhgsi/scripts.git && chmod u+x scripts/chiper/sync.sh && ./scripts/chiper/sync.sh
