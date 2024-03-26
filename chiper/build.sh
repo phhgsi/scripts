dca339daf90c6ba9711bcbaa81b2f928c93e0588
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-03-26 05:40:23 +0530

    Update build.sh

--- chiper/build.sh
+++ chiper/build.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+#envsetup
+source build/envsetup.sh
+# lunch command 
+lunch chiper_oscar-user
+export TZ=Asia/Dhaka
+#make command
+mka bacon
+# Check build status and display animation
