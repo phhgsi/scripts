b461426008c3f092f888074f52e6b800f0539b16
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-03-28 14:43:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 #envsetup
 source build/envsetup.sh
 # lunch command 
-lunch derp_oscar-user
+lunch evolution_oscar-userdebug
 export TZ=Asia/Dhaka
 #make command
-mka derp
+m evolution
 # Check build status and display animation
 
