59340f5ce5da1c709eede9b5747d0544715c898f
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-07-13 06:10:36 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -19,7 +19,6 @@ rm -rf vendor/oneplus/sm6375-common
 git clone https://github.com/phhgsi/vendor_oneplus_sm6375-common.git -b fourteen vendor/oneplus/sm6375-common
 rm -rf hardware/oplus
 git clone https://github.com/PixelOS-Devices/hardware_oplus.git -b fourteen-qpr2-holi hardware/oplus
-rm -rf prebuilts/clang/host/linux-x86/r450784d
 rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
 git clone https://gitlab.com/ImSurajxD/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
 export PATH="$HOME/tmp/src/android/prebuilts/clang/host/linux-x86/clang-r450784d/bin:$PATH"
@@ -27,8 +26,6 @@ rm -rf vendor/oplus/lib
 git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
 rm -rf vendor/oplus/camera
 git clone https://gitlab.com/ajay24ce008/proprietary_vendor_oplus_camera.git -b lineage-21-op12 vendor/oplus/camera
-rm -rf prebuilts/derp
-git clone https://github.com/derpfestoscar/prebuilts_derp.git prebuilts/derp
 rm -rf vendor/derp
 git clone https://github.com/selfmusing/vendor_derp.git vendor/derp
 rm -rf hardware/google/pixel/kernel_headers
