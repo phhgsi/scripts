0163accbcf7dd8f1dbf3483410292d2dfb8e582a
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-10-24 00:27:06 -0700

    Update pineapple.sh

--- pineapple.sh
+++ pineapple.sh
@@ -1,1 +1,18 @@
+rm -rf .repo/local_manifests/ hardware/google/pixel/kernel_headers
+#repo init rom
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+#local manifest
+git clone  https://github.com/OnePlus-Nord4-devs/local_manifests.git --depth 1 -b derp .repo/local_manifests &&
+# Sync 
+repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+#build
+/opt/crave/resync.sh
+source build/envsetup.sh
+export BUILD_USERNAME=phhgsi 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
+export TZ=Asia/india
+# Choose a target device 
+source build/envsetup.sh
+lunch lineage_oscar-userdebug && make installclean && mka bacon
 
