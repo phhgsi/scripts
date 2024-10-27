e086ac8b256f724506dd99d0fcc352daf3e9a5cb
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-10-27 08:55:00 +0530

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Directory containing the symlinks
-SYMLINK_DIR="/kernel"
-
-# Loop through each symlink in the directory
-find "$SYMLINK_DIR" -type l | while read symlink; do
-    # Get the target file the symlink points to
+# Loop through all symlinks in the current directory and subdirectories
+find . -type l | while read symlink; do
+    # Get the target of the symlink
     target=$(readlink "$symlink")
 
     # Check if the target exists
     if [ -e "$target" ]; then
-        # Copy the target file to the symlink's directory
-        cp "$target" "$SYMLINK_DIR"
-        
-        # Recreate the symlink (this will replace it)
-        ln -sf "$(basename "$target")" "$symlink"
+        # Copy the target to the location of the symlink
+        cp -r "$target" "$(dirname "$symlink")/"
+        # Remove the symlink
+        rm "$symlink"
+    else
+        echo "Target for symlink $symlink does not exist: $target"
     fi
 done
