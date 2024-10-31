0c8e366957a56ebadb4e9a31e3f88aefb1c0ef83
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-10-31 21:31:43 +0530

    Update pineapple.sh

--- pineapple.sh
+++ pineapple.sh
@@ -1,4 +1,4 @@
-rm -rf .repo/local_manifests/ hardware/google/pixel/kernel_headers hardware/qcom-caf/sm8650/audio/pal vendor/qcom/opensource/dataservices hardware/qcom-caf/wlan 
+rm -rf .repo/local_manifests/ hardware/google/pixel/kernel_headers hardware/qcom-caf/sm8650/audio/pal vendor/qcom/opensource/dataservices hardware/qcom-caf/wlan hardware/qcom-caf/sm8650/audio/primary-hal
 #local manifest
 git clone  https://github.com/OnePlus-Nord4-devs/local_manifests.git --depth 1 -b derp .repo/local_manifests &&
 rm -rf device/oneplus/avalon-prebuilt
