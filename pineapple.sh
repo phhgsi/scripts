ccdd8d5f3e17d09e3145d5777ddad9063f5ea84b
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-11-03 19:22:29 +0530

    Update pineapple.sh

--- pineapple.sh
+++ pineapple.sh
@@ -8,11 +8,12 @@ repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
 #build
 /opt/crave/resync.sh
 source build/envsetup.sh
+export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_USERNAME=phhgsi 
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export TZ=Asia/india
 # Choose a target device 
 source build/envsetup.sh
-lunch lineage_avalon-ap2a-userdebug && make installclean && mka bacon
+lunch lineage_avalon-ap2a-userdebug && make clean && mka bacon
 
