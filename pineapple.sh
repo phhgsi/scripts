654b8ced002fc6cbd56eabe59d852bef97e70150
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-11-04 12:44:13 +0530

    Update pineapple.sh

--- pineapple.sh
+++ pineapple.sh
@@ -16,5 +16,5 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
 export TZ=Asia/india
 # Choose a target device 
 source build/envsetup.sh
-lunch lineage_avalon-ap2a-userdebug && make clean && mka bacon
+lunch lineage_avalon-ap2a-userdebug && mka bacon
 
