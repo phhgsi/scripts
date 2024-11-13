ebfb7676643eb355f9b4c51f5b065fbe6611a98c
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-11-13 20:57:42 +0530

    Update pineapple.sh

--- pineapple.sh
+++ pineapple.sh
@@ -1,14 +1,12 @@
-rm -rf .repo/local_manifests/ hardware/google/pixel/kernel_headers hardware/qcom-caf/sm8650/audio/pal vendor/qcom/opensource/dataservices hardware/qcom-caf/wlan hardware/qcom-caf/sm8650/audio/primary-hal hardware/oplus/KeyHandler
+rm -rf .repo/local_manifests/ hardware/google/pixel/kernel_headers 
 #local manifest
 git clone  https://github.com/OnePlus-Nord4-devs/local_manifests.git --depth 1 -b derp .repo/local_manifests &&
 rm -rf device/oneplus/avalon-prebuilt
 git clone https://www.opencode.net/romdumprx/Prebuilt_kernel_pineapple.git device/oneplus/avalon-prebuilt
-git clone https://github.com/OnePlus-Nord4-devs/android_kernel_oneplus_sm7675-modules.git kernel/module/sm8650-common
 # Sync 
 repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
 #build
 /opt/crave/resync.sh
-rm -rf hardware/oplus/KeyHandler
 source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_USERNAME=phhgsi 
