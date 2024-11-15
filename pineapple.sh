727baf6a424d2046a69d3749734b4e9e2b6ad6d7
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-11-15 13:17:22 +0530

    Update pineapple.sh

--- pineapple.sh
+++ pineapple.sh
@@ -8,10 +8,12 @@ repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
 #build
 /opt/crave/resync.sh
 source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_USERNAME=phhgsi 
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=1
+
 export TZ=Asia/india
 # Choose a target device 
 source build/envsetup.sh
