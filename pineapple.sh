b638f89946cff1f440b7b5e7ce1cc6413f79ac71
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2024-11-21 21:26:54 +0530

    Update pineapple.sh

--- pineapple.sh
+++ pineapple.sh
@@ -1,14 +1,8 @@
 rm -rf .repo/local_manifests/ hardware/google/pixel/kernel_headers kernel/oneplus/sm8650 kernel/oneplus/sm8650-modules
+#yaap repo
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 #local manifest
 git clone  https://github.com/OnePlus-Nord4-devs/local_manifests.git --depth 1 -b derp .repo/local_manifests &&
-
-# Sync
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
-rm -rf device/oneplus/avalon-prebuilt
-git clone https://github.com/yaap/device_oneplus_waffle-kernel.git device/oneplus/avalon-kernel
-rm -rf kernel/oneplus/sm8650
-git clone https://github.com/yaap/kernel_oneplus_sm8650.git kernel/oneplus/sm8650
-#build
 /opt/crave/resync.sh
 rm -rf hardware/oplus/KeyHandler
 source build/envsetup.sh
